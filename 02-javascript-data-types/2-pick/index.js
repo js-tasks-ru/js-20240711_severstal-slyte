ed32686e8d6ccb418e8e61d8b74c65399f7ce384
TatyanaSlyandina67 <slyte@severstal.com>
2024-07-22 15:35:40 +0300

    Модуль Основы JS. Задача 2

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,10 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+    const picknew = {};
+    for (const field of fields) {
+    picknew[field] = obj[field];
+    }
+    return picknew;
+    
 };
