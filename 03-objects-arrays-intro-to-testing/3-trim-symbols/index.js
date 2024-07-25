c6883485b2c0188c2aa343f926d97b09a8c8bd86
TatyanaSlyandina67 <slyte@severstal.com>
2024-07-25 13:00:26 +0300

    Задание 3. Задача 3.trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,26 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+        let newStr ='';
+        let preStr='';
+        let i=1; 
+        if (size === undefined) {
+            newStr = string;
+          } else { 
+            if (size>0) {
+        for (let char of string) 
+          {
+           if (i<size || preStr!=char) {
+             newStr=newStr+char;
+             if (preStr!=char){
+                 preStr=char;
+                 i=1;
+             } else if (i<size) {
+              i=i+1;
+            }
+           }
+          }
+        }
+    }   
+    return newStr;
 }
